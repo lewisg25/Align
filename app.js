cc68591b2ae4f1006ba27eb2cd76d3f0c65fd168
Lewis G <lewis.garnett96@yahoo.com>
2026-02-22 21:34:18 -0500

    added the features for the middle part of the homepage, updated the stlyes.css and fixed the dark mode toogel to cover the entire homepage, had to update the app.js

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-// THEME TOGGLE
+// // THEME TOGGLE
+// const toggleBtn = document.getElementById("themeToggle");
+// const body = document.body;
+
+// // Load saved theme
+// if (localStorage.getItem("theme") === "light") {
+//   body.classList.add("light-mode");
+//   toggleBtn.textContent = "☀️";
+// }
+
+// // Toggle on click
+// toggleBtn.addEventListener("click", () => {
+//   body.classList.toggle("light-mode");
+
+//   if (body.classList.contains("light-mode")) {
+//     toggleBtn.textContent = "☀️";
+//     localStorage.setItem("theme", "light");
+//   } else {
+//     toggleBtn.textContent = "🌙";
+//     localStorage.setItem("theme", "dark");
+//   }
+// });
+
 const toggleBtn = document.getElementById("themeToggle");
-const body = document.body;
 
-// Load saved theme
-if (localStorage.getItem("theme") === "light") {
-  body.classList.add("light-mode");
-  toggleBtn.textContent = "☀️";
-}
-
-// Toggle on click
 toggleBtn.addEventListener("click", () => {
-  body.classList.toggle("light-mode");
+  document.body.classList.toggle("light-mode");
 
-  if (body.classList.contains("light-mode")) {
+  // Change icon
+  if (document.body.classList.contains("light-mode")) {
     toggleBtn.textContent = "☀️";
-    localStorage.setItem("theme", "light");
   } else {
     toggleBtn.textContent = "🌙";
-    localStorage.setItem("theme", "dark");
   }
 });
