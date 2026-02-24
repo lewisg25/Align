2a145d5141df1677ce5da83c73b509cf21f9a594
Lewis G <lewis.garnett96@yahoo.com>
2026-02-23 21:29:00 -0500

    removed the toogle for dark mode from the app.js file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 
-const toggleBtn = document.getElementById("themeToggle");
+// const toggleBtn = document.getElementById("themeToggle");
 
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+// toggleBtn.addEventListener("click", () => {
+//   document.body.classList.toggle("light-mode");
 
-  // Change icon
-  if (document.body.classList.contains("light-mode")) {
-    toggleBtn.textContent = "☀️";
-  } else {
-    toggleBtn.textContent = "🌙";
-  }
-});
+//   // Change icon
+//   if (document.body.classList.contains("light-mode")) {
+//     toggleBtn.textContent = "☀️";
+//   } else {
+//     toggleBtn.textContent = "🌙";
+//   }
+// });
