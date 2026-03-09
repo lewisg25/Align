0d2f2f2a14fe2935454f6969a22806a6bd569609
Lewis G <lewis.garnett96@yahoo.com>
2026-03-08 22:14:27 -0400

    updated the programs page in the contact page will work on the get started in the login page

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+const toggleBtn = document.getElementById("toggle-mode");
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 
-// const toggleBtn = document.getElementById("themeToggle");
-
-// toggleBtn.addEventListener("click", () => {
-//   document.body.classList.toggle("light-mode");
-
-//   // Change icon
-//   if (document.body.classList.contains("light-mode")) {
-//     toggleBtn.textContent = "☀️";
-//   } else {
-//     toggleBtn.textContent = "🌙";
-//   }
-// });
-
-
+  // Optional: Change the button text based on the current mode
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "Dark Mode";
+  } else {
+    toggleBtn.textContent = "Light Mode";
+  }
+});
